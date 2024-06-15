7d410cede4ccdceb4382afcf461b31306184dd32
Leyder2023 <151747360+Leyder2023@users.noreply.github.com>
2024-06-14 23:49:33 -0500

    Add files via upload

---
+++ estructurasSimplesYDobles1/src/main/java/com/mycompany/estructurassimplesydobles1/EstructurasSimplesYDobles1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.estructurassimplesydobles1;
+
+/**
+ *
+ * @author john
+ */
+public class EstructurasSimplesYDobles1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
